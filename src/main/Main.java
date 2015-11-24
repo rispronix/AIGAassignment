d648e5be0412691cafb06b8d0ace9a0c724f1b6d
rispronix <richard.rowley@live.com>
2015-11-24 12:20:12 +0000

    output refactoring

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,6 @@ import ga.parameterSearch.ProbabilitySearch;
 import ga.tsp.TSPGA;
 
 import java.util.Random;
-import recombination.PermutationRecombination;
 
 /**
  *
@@ -37,30 +36,21 @@ public class Main {
         new AverageNRuns(10, new Function3(seed, 100, 100, 0.9, 0.5, 10));
 
         System.out.print("\nExperiment 5: Function3 with merge recombination (10 variables)");
-        new AverageNRuns(10,
-                new Function3MergeRecombination(seed, 100, 100, 0.9, 0.5, 10));
+        new AverageNRuns(10, new Function3MergeRecombination(seed, 100, 100, 0.9, 0.5, 10));
 
-        System.out.print("\nExperiment 6: Function3 with best of four tournament "
-                + "selection and merge recombination (10 variables)");
-        new AverageNRuns(10,
-                new Function3BestOfFourSelection(seed, 100, 100, 0.9, 0.5, 10));
+        System.out.print("\nExperiment 6: Function3 with best of four tournament selection and merge recombination (10 variables)");
+        new AverageNRuns(10, new Function3BestOfFourSelection(seed, 100, 100, 0.9, 0.5, 10));
 
-        System.out.print("\nExperiment 7: Function3 with best of four tournament "
-                + "selection and merge recombination (20 variables)");
-        new AverageNRuns(10,
-                new Function3BestOfFourSelection(seed, 100, 100, 0.9, 0.5, 20));
+        System.out.print("\nExperiment 7: Function3 with best of four tournament selection and merge recombination (20 variables)");
+        new AverageNRuns(10, new Function3BestOfFourSelection(seed, 100, 100, 0.9, 0.5, 20));
 
-        System.out.print("\nExperiment 8: probability parameter search on function"
-                + " 3 with best of four selection and merge recombination (20 variables)");
-        new ProbabilitySearch(seed, 10, 100, 0.9, 0.5,
-                new Function3BestOfFourSelection(seed,
-                        100, 100, 0.9, 0.8, 20)).run();
-        
+        System.out.print("\nExperiment 8: probability parameter search on function 3 with best of four selection and merge recombination (20 variables)");
+        new ProbabilitySearch(seed, 10, 100, 0.9, 0.5, new Function3BestOfFourSelection(seed, 100, 100, 0.9, 0.8, 20)).run();
+
         System.out.print("\nExperiment 9: TSPGA");
-       new AverageNRuns(10, new TSPGA(seed,10));
-       
+        new AverageNRuns(10, new TSPGA(seed, 10));
+
         System.out.print("\nExperiment 10: probability parameter search on TSPGA");
-        new ProbabilitySearch(seed, 10, 100, 0.9, 0.5,
-               new TSPGA(seed,10)).run();
+        new ProbabilitySearch(seed, 10, 100, 0.9, 0.5, new TSPGA(seed, 10)).run();
     }
 }
